070fff3730c22ec1ceeae1f6f178c4f78d8b4b2a
Saksham Jain <sksmlabs@Sakshams-MacBook-Pro.local>
2025-08-02 13:15:05 +0530

    establishing connection to solana local rpc

--- src/main.rs
+++ src/main.rs
@@ -1,4 +1,11 @@
+use solana_client::rpc_client::RpcClient;
+
 #[tokio::main]
 async fn main() {
     println!("\n======== Creating connecting to local Solana RPC ========");
+
+    let rpc_url = "https://127.0.0.1:8899";
+    let client = RpcClient::new(rpc_url.to_string());
+    println!("\nConnected to Solana RPC at localhost:8899");
+    
 }
